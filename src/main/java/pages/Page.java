7ad3ffa523afc18295c73edd305764f98c400338
Yodude2002 <Yococommand@gmail.com>
2019-06-05 10:55:04 -0400

    Added more classes

--- src/main/java/pages/Page.java
+++ src/main/java/pages/Page.java
@@ -2,11 +2,29 @@ package pages;
 
 import engine.Main;
 
+/**
+ * This interface specifies the methods that all Pages must have.
+ *
+ * In addition, all Pages must have a default constructor.
+ * Only the default constructor will be called.
+ */
 public interface Page {
 
+    /**
+     * This method is called by {@link Main} on every frame that this Page is open
+     * @param main the {@link processing.core.PApplet} for the application
+     */
     void draw(Main main);
 
+    /**
+     * This method is called by {@link Main} every time this Page is clicked
+     * @param main the {@link processing.core.PApplet} for the application
+     */
     void onMouseEvent(Main main);
 
+    /**
+     * This method is called by {@link Main} every time a key is pressed and this Page is open
+     * @param main the {@link processing.core.PApplet} for the application
+     */
     void onKeyPress(Main main);
 }
