b89416c52bb41f3fdef051d93fd1e0afd21caff6
Yodude2002 <Yococommand@gmail.com>
2019-06-05 10:18:23 -0400

    Modified the base framework.

---
+++ src/main/java/pages/MillikanPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import engine.Main;
+
+public class MillikanPage implements Page {
+    @Override
+    public void draw(Main main) {
+
+    }
+
+    @Override
+    public void onMouseEvent(Main main) {
+
+    }
+
+    @Override
+    public void onKeyPress(Main main) {
+
+    }
+}
---
+++ src/main/java/pages/ShrodingerPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import engine.Main;
+
+public class ShrodingerPage implements Page {
+    @Override
+    public void draw(Main main) {
+
+    }
+
+    @Override
+    public void onMouseEvent(Main main) {
+
+    }
+
+    @Override
+    public void onKeyPress(Main main) {
+
+    }
+}
