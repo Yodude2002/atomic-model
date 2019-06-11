c748b028352c5d2907d71b6079bb083e729c87ca
regenerativep <regeneralp@gmail.com>
2019-06-11 07:01:04 -0400

    more chadwick

--- src/main/java/pages/ChadwickPage.java
+++ src/main/java/pages/ChadwickPage.java
@@ -12,15 +12,17 @@ public class ChadwickPage extends Page {
         uiManager.addElement(new ImageElement("chadwick.jpeg",0,50,this));
         uiManager.addElement(new TextElement("Discoverer of the Neutron",0xffffffff,12, 0,330));
         uiManager.addElement(new TextElement(""+
-                "The Bohr model was not created to account for the existence of a neutron particle.\n" +
-                "Chadwick fired alpha particles at beryllium, with the beryllium followed by paraffin\n" +
-                "wax, and then a detector that could detect protons. He discovered that there was some\n" +
-                "neutral particle with mass slightly higher than a proton being released from the\n" +
-                "beryllium because the paraffin wax was emitting protons, and named those particles\n" +
-                "neutrons.\n" +
-                "Chadwick then modified the Bohr model to account for neutrons in the nucleus.\n" +
-                "The discovery of the neutron dramatically accelerated nuclear physics and his research\n" +
-                "was used on the allied side during World War 2.",
+                "Chadwick worked at Manchester University with Rutherford in 1921. There, he created a certain\n" +
+                "experiment that resulted in the discovery of the neutron.\n" +
+                "   In the experiment, alpha particles were first fired at some beryllium. Placed\n" +
+                "after the beryllium is some paraffin wax, and then after the paraffin wax there\n" +
+                "is a geiger counter. For the geiger counter to detect something means that there\n" +
+                "is some particle going between beryllium and the paraffin wax after firing alpha particles\n" +
+                "at the beryllium in order for the paraffin wax to release protons.\n" +
+                "   This new particle discovered was then called the neutron, and has a mass slightly higher\n" +
+                "than a proton. He added the neutron to Bohr's model of the atom.\n" +
+                "   His work accelerated the development of nuclear physics for the allied side in World War 2.",
                 0xffffffff, 20, 208, 16));
+        uiManager.addElement(new ImageElement("chadwickexperiment.png", 256, 384, this));
     }
 }
