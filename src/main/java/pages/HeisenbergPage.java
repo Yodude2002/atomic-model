11671cd7fdbd40ee1e9198bc4a654a3362dadec8
nathandorson <32528988+nathandorson@users.noreply.github.com>
2019-06-11 06:17:59 -0400

    Its better

--- src/main/java/pages/HeisenbergPage.java
+++ src/main/java/pages/HeisenbergPage.java
@@ -1,14 +1,25 @@
 package pages;
 
+import engine.engineElements.ImageElement;
 import engine.engineElements.TextElement;
 
 public class HeisenbergPage extends Page {
     public HeisenbergPage()
     {
         super(0xff000000);
-        uiManager.addElement(new TextElement("Heisenberg", 0xffffffff, 70,40, 10));
+        uiManager.addElement(new TextElement("Werner Heisenberg", 0xffffffff, 70,40, 10));
+        uiManager.addElement(new ImageElement("Heisenberg.jpg", 995, 10, 1.4f, this));
+        uiManager.addElement(new ImageElement("ProbFields.jpg", 300, 450, this));
         uiManager.addElement(new TextElement("" +
-                "[text]" +
+                "The creation of the Bohr model was a fantastic step forward for the model of the atom, Heisenberg was\n" +
+                "a little *uncertain* about the rigidity of the Bohr model. Heisenberg saw the claim by the Bohr model that\n" +
+                "electrons had fixed orbits around a nucleus to be not quite right. Nothing would be that perfect.\n" +
+                "   And, as it turned out, he was right! Using just a little bit of theoretical quantum math, Heisenberg\n" +
+                "deduced that the more accurately the position of a particle is known, the less is known about its momentum,\n" +
+                "and vice versa.\n" +
+                "   Thanks to Heisenberg's uncertainty principle, the accepted model of the atom was updated to incorporate\n" +
+                "probability fields, giving possible locations for electrons at any given moment, rather than precise orbital\n" +
+                "paths. This work would go on to build the foundation for quantum mechanics." +
                 "", 0xffffffff, 20,20,100));
     }
 }
--- src/main/java/pages/ShrodingerPage.java
+++ src/main/java/pages/ShrodingerPage.java
@@ -1,14 +1,26 @@
 package pages;
 
+import engine.engineElements.ImageElement;
 import engine.engineElements.TextElement;
 
 public class ShrodingerPage extends Page {
     public ShrodingerPage()
     {
         super(0xff000000);
-        uiManager.addElement(new TextElement("Shrödinger", 0xffffffff, 70,40, 10));
+        uiManager.addElement(new TextElement("Erwin Shrödinger", 0xffffffff, 70,40, 10));
+        uiManager.addElement(new ImageElement("Shrod.jpg", 1025, 10, 0.5f, this));
+        uiManager.addElement(new ImageElement("WaverPart.jpg", 300, 450, 0.4f, this));
         uiManager.addElement(new TextElement("" +
-                "[text]" +
+                "Shrödinger was perhaps the most influential scientist when it comes to the modern understanding of the atom.\n" +
+                "He recognized that the atomic model could not be made black and white, and, like Heisenberg, he developed\n" +
+                "a set of theories that accounted for the nebulous nature of the world at an atomic level.\n" +
+                "   Shrödinger's work made bold claims about atomic and quantum theory, but it was all grounded in facts and\n" +
+                "empirical evidence found by other scientists such as Einstein or Planck. Shrödinger's most important idea\n" +
+                "was that light acts like both a wave and a particle. This concept of Wave-Particle Duality also applied to\n" +
+                "electrons, and gave an explanation for previous models of the atom. Shrödinger also claimed that electrons have\n" +
+                "angular momentum, or \"spin\". With all of these new ideas, Shrödinger pieced together an equation that describes\n" +
+                "the wave function of a quantum-mechanical system, and in doing so, bridged the gap between Newtonian and\n" +
+                "quantum physics." +
                 "", 0xffffffff, 20,20,100));
     }
 }
